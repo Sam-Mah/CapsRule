589c54ca2feff682bdae57df4ea51a021cb724b7
nt-coder <47539163+ntt-coder@users.noreply.github.com>
2021-02-04 23:25:26 -0400

    Update Vector_Instantiation_Dynamic_Layers.py

--- Vector_Instantiation_Dynamic_Layers.py
+++ Vector_Instantiation_Dynamic_Layers.py
@@ -2,7 +2,6 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import math
-# from Rule_Extraction_And import extract_rules_boundary
 from Rule_Extraction_and_or import extract_rules_boundary, mean, std
 from Rule_Evaluation import evaluate_rules_boundary
 from Rule_Validation import validate_rules
@@ -21,116 +20,24 @@ init_sigma = 1.0
 CONSTANTS
 '''
 # # @@@@@@@@@@@@@@@@@@FFCN Constants@@@@@@@@@@@@@@@@@@@@@@@@
-# &&&&&&&&&&&&&&&&vertebral&&&&&&&&&&&&&&&&&&&
-# N_CLASSSES = 2
-# input = 6
-# caps1_n_caps = 4
-# caps2_n_caps = N_CLASSSES
-# batch_size = 5
-# caps1_dim = 8
-# caps2_dim = 6
-# Routing_Iter = 2
-# LRATE = 0.001
-# n_epochs = 50
-# &&&&&&&&&&&&IRIS&&&&&&&&&&&&&&
-# N_CLASSSES = 2
-# input = 4
-# caps1_n_caps = 3
-# caps2_n_caps = N_CLASSSES
-# batch_size = 5
-# caps1_dim = 8
-# caps2_dim = 6
-# Routing_Iter = 2
-# LRATE = 0.001
-# n_epochs = 50
-# @@@@@@@@@@@@@@@@@@Glass@@@@@@@@@@@@@@@@@@@@@@@@
-# N_CLASSSES = 7
-# input = 9
-# caps1_n_caps = 8
-# caps2_n_caps = N_CLASSSES
-# batch_size = 10
-# caps1_dim = 20
-# caps2_dim = 30
-# Routing_Iter = 2
-# LRATE = 0.01
-# n_epochs = 50
-# &&&&&&&&&&&&&&&&ecoli2&&&&&&&&&&&&&&&&&&&
-# N_CLASSSES = 5
-# input = 7
-# caps1_n_caps = 8
-# caps2_n_caps = N_CLASSSES
-# batch_size = 5
-# caps1_dim = 8
-# caps2_dim = 6
-# Routing_Iter = 2
-# LRATE = 0.001
-# n_epochs = 50
-# &&&&&&&&&&&&&&&&CICDOS2019 Full Features&&&&&&&&&&&&&&&&&&&
-# N_CLASSSES = 2
-# input = 82
-# caps1_n_caps = 40
-# caps2_n_caps = N_CLASSSES
-# batch_size = 20
-# caps1_dim = 30
-# caps2_dim = 20
-# Routing_Iter = 2
-# LRATE = 0.05
-# n_epochs = 10
 # &&&&&&&&&&&&&&&&CICDOS2019-12 Features&&&&&&&&&&&&&&&&&&&
-batch_size = 50
+batch_size = 100
 N_CLASSSES = 4
 num_layers = 5
 size_layers = [25, 20, 15, 8, N_CLASSSES] #output_dimesion for each layer
 caps_dim = [30, 25, 20, 15, 10]
-# num_layers = 4
-# size_layers = [20, 15, 8, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [25, 20, 15, 10]
-# num_layers = 4
-# size_layers = [24, 18, 10, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [28, 24, 18, 10]
-# num_layers = 5
-# size_layers = [24, 20, 14, 5, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [29, 25, 19, 10, 8]
-# num_layers = 6
-# size_layers = [26, 21, 16, 11, 6, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [32, 27, 23, 18, 11, 7]
-# num_layers = 7
-# size_layers = [26, 22, 18, 14, 10, 6, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [35, 30, 25, 20, 15, 10, 8]
-# num_layers = 3
-# size_layers = [20, 10, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [25, 15, 10]
-# size_layers = [15, 12, N_CLASSSES] #output_dimesion for each layer
-# caps_dim = [20, 18, 16]
 caps_dim_input = 1
 input = 30
-# input = 20
-# caps1_n_caps = 6
-# caps2_n_caps = N_CLASSSES
-# batch_size = 5
-# caps1_dim = 10
-# caps2_dim = 8
-Routing_Iter = 2
-# LRATE = 1e-5
-# start_lr = 0.1
+Routing_Iter = 3
 LRATE = 0.1
-n_epochs = 150
-# Small epsilon value for the BN transform
-# epsilon = 1e-3
+n_epochs = 300
 loss_thresh = 0.01
 restore_checkpoint = True
 # $$$$$$$$$$$$$$$$$Decoder Constants$$$$$$$$$$$$$$$$$$$$$$$
-# n_hidden1 = caps_dim[-1]
-# n_hidden2 = math.floor((caps_dim[-1]+input)/2)
-# n_hidden1 = 100
 n_hidden1 = 5
 n_hidden2 = 15
 n_hidden3 = 20
 n_output = input
-# n_hidden1 = 5
-# n_hidden2 = 10
-# n_hidden3 = 15
-# n_output = input
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 def condition(d, caps_predicted, routing_weights, raw_weights, caps1_n,caps2_n, counter):
@@ -142,21 +49,6 @@ def loop_body(caps_output, caps_predicted, routing_weights, raw_weights, caps1_n
     weighted_predictions = tf.multiply(routing_weights, caps_predicted, name="weighted_predictions")
     '''s'''
     weighted_sum = tf.reduce_sum(weighted_predictions, axis=1, keep_dims=True, name="weighted_sum")
-
-    '''Batch Normalization'''
-    # Calculate batch mean and variance
-    # batch_mean, batch_var = tf.nn.moments(weighted_sum,[-3])
-    # batch_mean_expanded = tf.expand_dims(batch_mean,-3)
-    # batch_var_expanded = tf.expand_dims(batch_var, -3)
-    # batch_mean_tiled = tf.tile(batch_mean_expanded,[1,1,caps2_n,1,1])
-    # batch_var_tiled = tf.tile(batch_var_expanded, [1, 1, caps2_n, 1, 1])
-    # # Apply the initial batch normalizing transform
-    # z_hat = tf.divide(tf.subtract(weighted_sum, batch_mean_tiled), tf.sqrt(tf.add(batch_var_tiled, epsilon)))
-    #
-    # # Scale and shift to obtain the final output of the batch normalization
-    # # this value is fed into the activation function (here a sigmoid)
-    # weighted_sum = tf.add(tf.matmul(z_hat,scale),beta)
-    # # BN = tf.layers.batch_normalization(weighted_sum,momentum=0.9)
     # '''Squash the output 𝐯𝑗=squash(𝐬𝑗) '''
     caps_output = squash(weighted_sum, axis=-2, name="caps_output")
     # caps_output = tf.nn.relu(BN, name="caps_output")
@@ -185,16 +77,9 @@ def caps_layer(input_size, output_size, caps_dim_input,caps_dim_output):
                       'weights': tf.Variable(tf.random_normal(shape=(1, input_size, output_size, caps_dim_output, caps_dim_input)
                                                               , stddev=init_sigma, mean=0.0, dtype=tf.float32, name='weights')
                                              , name='weights'),
-                      # 'weights': tf.get_variable("weights", shape=[1, input_size, output_size, caps_dim_output, caps_dim_input],
-                      #                            initializer=xavier_init(input_size,output_size)),
-                      # 'biases': tf.Variable(tf.random_normal([caps1_n_caps])),
                       'raw_weights': tf.Variable(tf.zeros([batch_size, input_size, output_size, 1, 1],
                                                           dtype=np.float32, name="raw_weights")),
-                      # Create two new parameters, scale (gamma) and beta (shift) for batch normalization
-                      # 'scale': tf.Variable(tf.ones([batch_size,1,output_size,1,1])),
-                      # 'beta': tf.Variable(tf.zeros([batch_size,1,output_size,1,1]))
                       }
-
     return hidden_layer
 
 def neural_network_model(data):
@@ -275,9 +160,6 @@ def decoder(decoder_input):
         hidden3 = tf.layers.dense(hidden2, n_hidden3,
                                   activation=tf.nn.relu,
                                   name="hidden3")
-        # hidden4 = tf.layers.dense(hidden3, n_hidden4,
-        #                           activation=tf.nn.relu,
-        #                           name="hidden4")
         decoder_output = tf.layers.dense(hidden3, n_output,
                                          activation=tf.nn.sigmoid,
                                          name="decoder_output")
@@ -302,40 +184,16 @@ def next_batch(data, batch_indx):
 
     return lst_data, lst_lbl
 
-def tweak_pose_parameters(output_vectors, min=-0.5, max=0.5, n_steps=11):
-    steps = np.linspace(min, max, n_steps) # -0.25, -0.15, ..., +0.25
-    pose_parameters = np.arange(n_hidden1) # 0, 1, ..., 15
-    tweaks = np.zeros([n_hidden1, n_steps, 1, 1, 1, n_hidden1, 1])
-    tweaks[pose_parameters, :, 0, 0, 0, pose_parameters, 0] = steps
-    output_vectors_expanded = output_vectors[np.newaxis, np.newaxis]
-    return tweaks + output_vectors_expanded
-
 '''                 
 Reading the data
 '''
-def normalize(data):
 
-    # normalized_data = Normalizer(norm='l2').fit_transform(data)
-    normalized_data = minmax_scale(data)
-
-    return normalized_data
-
-# data = np.genfromtxt("Datasets/20_Features_CICDOS_Testing_balanced_80k.csv", delimiter=",")
-# data = np.genfromtxt("Datasets/Exploitation_ch3_15k.csv", delimiter=",")
-# data = np.genfromtxt("Datasets/Exploitation_ch3_15k.csv", delimiter=",")
-
-with open("Datasets/Reflection_ch1_20k.csv") as csv_file:
-# with open("Datasets/Exploitation_ch3_15k.csv") as csv_file:
+with open("Datasets/...") as csv_file:
     csv_reader = pd.read_csv(csv_file, delimiter=',')
 np_cols = csv_reader.columns.to_numpy()
 features_test = np.array([x.strip() for x in np_cols])
 np.savetxt("Experiments/Features.csv", features_test, delimiter=",", fmt='%s')
 data = csv_reader.to_numpy()
-# data = np.genfromtxt("Datasets/30_Features_CICDOS_Testing_balanced_LDAPMSSQLNetBios_15k.csv", delimiter=",")
-
-# print("mean input data:",np.mean(data),"std input data:",np.std(data))
-# data = np.genfromtxt("Datasets/glass.csv", delimiter=",")
-# data = np.genfromtxt("Datasets/iris.csv", delimiter=",")
 
 col_num = data.shape[1]
 row_num = data.shape[0]
@@ -345,10 +203,9 @@ labels = labels.astype(int)
 print(np.unique(labels))
 # dropping the labels' columns
 data = data[:, :col_num - 1]
-# data = normalize(data)
 ''' 1-d array to one-hot conversion
-# Labels 1 >>> 1 0 >>> Positive (Malicious)
-# Labels 2 >>> 0 1 >>> Negative (Benign)
+# Labels 1 >>> 1 0 >>> Positive
+# Labels 2 >>> 0 1 >>> Negative
 # In the rules, output 0 means (1 0)
 # In the rules, output 1 means (0 1)'''
 
@@ -381,7 +238,6 @@ squared_difference = tf.square(X_flat - decoder_out,
 reconstruction_loss = tf.reduce_mean(squared_difference,
                                     name="reconstruction_loss")
 
-# cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=tf.cast(prediction, tf.float32)))
 m_plus = 0.9
 m_minus = 0.1
 lambda_ = 0.5
@@ -400,8 +256,6 @@ L = tf.add(y * present_error, lambda_ * (1.0 - y) * absent_error,
 margin_loss = tf.reduce_mean(tf.reduce_sum(L, axis=1), name="margin_loss")
 '''Final Loss'''
 eta = 5.0e-15
-# eta = 5.0e-5
-# cost = tf.add(margin_loss, eta * reconstruction_loss, name="loss")
 cost = tf.add(margin_loss, eta * reconstruction_loss, name="loss")
 
 correct = tf.equal(tf.argmax(y, -1), tf.argmax(prediction, -1))
@@ -409,13 +263,6 @@ accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
 # --------------------------------------------------------------------------
 # Optimize
 # --------------------------------------------------------------------------
-# decayed_learning_rate = learning_rate *
-#                         decay_rate ^ (global_step / decay_steps)
-# global_step = tf.Variable(0, trainable=False, dtype= tf.int32)
-# learning_rate = tf.compat.v1.train.exponential_decay(
-#     start_lr, global_step=global_step,
-#     decay_steps=100, decay_rate=0.96)
-# optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost, global_step=global_step)
 optimizer = tf.train.AdamOptimizer(learning_rate=LRATE).minimize(cost)
 # --------------------------------------------------------------------------
 
@@ -438,15 +285,12 @@ teclass_labels = np.argmax(test_labels, axis=1)
 
 tmp_train = np.hstack((train_data, tclass_labels.reshape((len(train_labels), 1))))
 df_train = pd.DataFrame(tmp_train)
-# df_train.sort_values(df_train.columns.values[-1], inplace=True)
 
 tmp_val = np.hstack((val_data, vclass_labels.reshape((len(val_labels), 1))))
 df_val = pd.DataFrame(tmp_val)
-# df_val.sort_values(df_val.columns.values[-1], inplace=True)
 
 tmp_test = np.hstack((test_data, teclass_labels.reshape((len(test_labels), 1))))
 df_test = pd.DataFrame(tmp_test)
-# df_test.sort_values(df_test.columns.values[-1], inplace=True)
 
 n_iterations_per_epoch = len(train_data) // batch_size
 n_iterations_validation = len(val_data) // batch_size
@@ -458,20 +302,16 @@ with tf.Session() as sess:
         saver.restore(sess, checkpoint_path)
     else:
         init.run()
-    # summary_writer = tf.summary.FileWriter("./Capsnet_FFNN", sess.graph)
     epoch = 1
     loss_epochs = [2, 1]
     #Train until the number of maximum epochs or validation loss of two consecutive loops is less that loss threshold
     while (epoch < n_epochs or loss_epochs[epoch]-loss_epochs[epoch-1]> loss_thresh):
         '''Training'''
         rule_set = list()
-        # iteration_list = np.zeros(1)
-        # neural_network_cost_list = np.zeros(1)
-        # cond_order_set = list()
         btch_indx = 0
         for iteration in range(1, n_iterations_per_epoch + 1):
             X_batch, y_batch = next_batch(df_train, btch_indx)
-            # print(type(X_batch))
+           
             # Run the training operation and measure the loss:
             _, loss_train = sess.run(
                 [optimizer, cost],
@@ -585,10 +425,6 @@ with tf.Session() as sess:
         loss_epochs.append(loss_val)
         epoch +=  1
 
-
-global mean, std
-np.savetxt("Parameters/mean_couple.csv", mean, delimiter=",", fmt='%s')
-np.savetxt("Parameters/std_couple.csv", std, delimiter=",", fmt='%s')
 '''
 Evaluation
 '''
@@ -635,10 +471,6 @@ with tf.Session() as sess:
         rc_t.append(rc)
         f1_t.append(f1)
         btch_indx += batch_size
-        # np.savetxt("input.csv", dt.eval({X: np.transpose(X_batch), y: y_batch}), delimiter=",")
-        # np.savetxt("output.csv", prediction.eval({X: np.transpose(X_batch), y: y_batch}), delimiter=",")
-        # np.savetxt("real_output.csv", y.eval({X: np.transpose(X_batch), y: y_batch}), delimiter=",")
-
     loss_test = np.mean(loss_tests)
     acc_test = np.mean(acc_tests)
 
